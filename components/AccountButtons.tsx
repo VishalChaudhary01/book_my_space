5202afa0cc42f6365998f80cdf7553c51e35c13d
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-11-02 21:47:10 +0530

    udpate

--- components/AccountButtons.tsx
+++ components/AccountButtons.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
-import { checkoutPayment } from "@/app/actions/checkout";
 
 export function AccountButtons() {
-     const handleClick = async () => {
-          await checkoutPayment();
-     }
      return (
           <div className="grid grid-cols-2 gap-4 w-full md:w-2/3 lg:w-2/5">
-               <Button onClick={handleClick} className="w-full">Pay Bill Now</Button>
+               <Button className="w-full">Pay Bill Now</Button>
                <Button onClick={() => signOut()} className="w-full">
                     Logout
                </Button>
